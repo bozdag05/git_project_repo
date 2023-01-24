d35cfd29c7885c70b96efd04f3691f93db923456
bozdag05 <muhammadosmanov02@gmail.com>
2023-01-24 14:25:09 +0300

    Добавил текст в вывод

--- testing_git_from_consol.py
+++ testing_git_from_consol.py
@@ -1,8 +1,8 @@
 
 def git_test(BPLA):
     for info in BPLA:
-        return f'{info.name}\n' \
-               f'{info.speed}\n' \
-               f'{info.max_distance}\n' \
-               f'{info.max_hight}\n' \
-               f'{info.min_time} - {info.midle_time} - {info.max_time}'
+        return f'Название: {info.name}\n' \
+               f'Скорость: {info.speed}\n' \
+               f'МАКС Дистанция полёта: {info.max_distance}\n' \
+               f'Паталок: {info.max_hight}\n' \
+               f'Время полёта: {info.min_time} - {info.midle_time} - {info.max_time}'
