e6671188403fd196f2fdc0ee2e5549ae37519fb7
bozdag05 <muhammadosmanov02@gmail.com>
2023-01-24 14:05:35 +0300

    Класс для вывода информаций о БПЛА

--- testing_git_from_consol.py
+++ testing_git_from_consol.py
@@ -1,3 +1,8 @@
 
-def git_test():
-    pass
+def git_test(BPLA):
+    for info in BPLA:
+        return f'{info.name}\n' \
+               f'{info.speed}\n' \
+               f'{info.max_distance}\n' \
+               f'{info.max_hight}\n' \
+               f'{info.min_time} - {info.midle_time} - {info.max_time}'
